19332f548b983dde529952dffbabfb0acd641690
Vincent-ondeng <vincentondeng@gmail.com>
2022-09-29 10:03:24 +0300

    installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Installs a package
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem'
+# flask is installed using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
